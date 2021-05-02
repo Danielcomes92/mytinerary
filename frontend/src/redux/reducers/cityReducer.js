9d44f3d53e027317d4c33f5f40d9cc14e447c746
Daniel <dancomes92@gmail.com>
2021-05-02 15:09:42 -0300

    filter cities upgraded

--- frontend/src/redux/reducers/cityReducer.js
+++ frontend/src/redux/reducers/cityReducer.js
@@ -18,27 +18,19 @@ const cityReducer = (state = initialState, action) => {
         
         case 'FILTER_CITIES':
             let valueInput = action.payload.trim().toUpperCase()
-            if(valueInput) {
-                var citiesFiltered = state.cities.filter(city => (city.city).toUpperCase().includes(valueInput) && Array.from(city.city)[0].toUpperCase() === valueInput[0]);
-            }
-            if(citiesFiltered) {
-                if(citiesFiltered.length > 0) {
-                    return {
-                        ...state,
-                        citiesUpdated: citiesFiltered,
-                        noCitiesAlert: false
-                    }
-                } else {
-                    return {
-                        ...state,
-                        noCitiesAlert: true
-                    }
+            let citiesFiltered = state.cities.filter(city => (city.city).toUpperCase().indexOf(valueInput) === 0 && (city.city).toUpperCase().startsWith(valueInput));
+
+            if(valueInput.length > 0) {
+                return {
+                    ...state,
+                    citiesUpdated: citiesFiltered,
+                    noCitiesAlert: citiesFiltered.length > 0 ? false : true
                 }
             } else {
                 return {
                     ...state,
                     citiesUpdated: state.cities
-                }  
+                }
             }
 
         default:

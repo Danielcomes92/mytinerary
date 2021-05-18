94577e9fbe978f3c046e3a0bd849c43744cfa388
Daniel <dancomes92@gmail.com>
2021-05-18 03:05:09 -0300

    process env ok

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require('path')
 const app = express();
 
 const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8000;
 
 app.use(cors())
 app.use(express.json())
@@ -20,11 +20,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api', router)
 
 
-if(process.env.NODE_END === 'production') {
+if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-        // res.sendFile(path.join(__dirname+ "/client/build/index.html"))
+        // res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+        res.sendFile(path.join(__dirname+ "/client/build/index.html"))
     })
 }
 

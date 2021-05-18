4383b1b9baa283da77cce2f1a7a5a82d394712ef
Daniel <dancomes92@gmail.com>
2021-05-18 02:57:38 -0300

    last changes

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.use('/api', router)
 if(process.env.NODE_END === 'production') {
     app.use(express.static('client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname+ "/client/build/index.html"))
+        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+        // res.sendFile(path.join(__dirname+ "/client/build/index.html"))
     })
 }
-
 
 app.listen(port, host, () => {
     console.log(`App listening on port ${port} on ${host}`)

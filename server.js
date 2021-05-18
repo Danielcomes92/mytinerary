f4b6b53dfcee7572883ddd35e2eb2396d3e52419
Daniel <dancomes92@gmail.com>
2021-05-18 02:47:16 -0300

    port ok

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require('path')
 const app = express();
 
 const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 app.use(cors())
 app.use(express.json())

f82b42fd6eee81c5637b5bd557995df29e79bf3a
Daniel <dancomes92@gmail.com>
2021-06-08 17:34:39 -0300

    buildeo

--- frontend/src/components/city/CardCity.js
+++ frontend/src/components/city/CardCity.js
@@ -6,7 +6,7 @@ const CardCity = ({city}) => {
             <Link to={`/city/${city._id}`}>
                 <div className="w-full h65 cardCityContainer mx-auto flex flex-col bg-gray-100 shadow-md hover:shadow-lg transform transition duration-700 hover:scale-105">
                     <div className="w-full h65 bgCover bgCenter" style={{
-                        backgroundImage: `url('./img/${city.image}')`
+                        backgroundImage: `url('${city.image}')`
                     }}>
                         <p className="bg-gray-900 text-center w-full bg-opacity-90 text-white px-4 font-semibold lobster text-5xl md:text-5xl">{city.city}</p>
                     </div>
